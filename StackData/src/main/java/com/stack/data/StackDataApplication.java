a6ab6e7c5d0ddc1ca7502db8f9904299793e48bf
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2019-08-15 16:23:35 -0400

    Initial commit

---
+++ StackData/src/main/java/com/stack/data/StackDataApplication.java
@@ -0,0 +1,13 @@
+package com.stack.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackDataApplication.class, args);
+    }
+
+}
---
+++ StackData/src/test/java/com/stack/data/StackDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stack.data;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StackDataApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
