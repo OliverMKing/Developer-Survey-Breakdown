5fd7fed822cbc3e74ccb5abb18caadcba3eaae5d
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2019-08-17 16:36:07 -0400

    Loads and reads CSV file

--- StackData/src/main/java/com/stack/data/bootstrap/Bootstrap.java
+++ StackData/src/main/java/com/stack/data/bootstrap/Bootstrap.java
@@ -1,10 +1,15 @@
 package com.stack.data.bootstrap;
 
+import com.opencsv.CSVReader;
 import com.stack.data.api.v1.models.ResponseDTO;
 import com.stack.data.services.ResponseService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,6 +17,8 @@ import java.util.Set;
 public class Bootstrap implements CommandLineRunner {
 
     private final ResponseService responseService;
+    private final String CSV_URL = "C:\\Users\\Oliver\\Desktop\\survey_results_public.csv";
+    private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
 
     public Bootstrap(ResponseService responseService) {
         this.responseService = responseService;
@@ -19,36 +26,24 @@ public class Bootstrap implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        ResponseDTO responseDTO = new ResponseDTO();
-        responseDTO.setCountry("US");
-        responseDTO.setDevType("New grad");
-        Set<String> languages = new HashSet<>();
-        languages.add("Java");
-        languages.add("Python");
-        responseDTO.setLanguages(languages);
-        Set<String> frameworks = new HashSet<>();
-        frameworks.add("Spring");
-        frameworks.add("Rails");
-        responseDTO.setFrameworks(frameworks);
-        responseService.createNewResponse(responseDTO);
+        loadCSVData();
+    }
 
-        ResponseDTO responseDTO2 = new ResponseDTO();
-        responseDTO2.setCountry("CA");
-        responseDTO2.setDevType("New grad");
-        Set<String> languages2 = new HashSet<>();
-        languages2.add("Java");
-        languages2.add("C++");
-        responseDTO2.setLanguages(languages2);
-        Set<String> frameworks2 = new HashSet<>();
-        frameworks2.add("Spring");
-        frameworks2.add("React");
-        responseDTO2.setFrameworks(frameworks2);
-        responseService.createNewResponse(responseDTO2);
+    private void loadCSVData() {
+        logger.info("Loading data from CSV");
 
-        ResponseDTO responseDTO3 = new ResponseDTO();
-        responseDTO3.setCountry("CA");
-        responseDTO3.setDevType("5 Years Experience");
-        responseDTO3.setFrameworks(frameworks2);
-        responseService.createNewResponse(responseDTO3);
+        CSVReader reader = null;
+        try {
+            reader = new CSVReader(new FileReader(CSV_URL));
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                System.out.println(line[6]);
+            }
+            logger.info("Done loading data from CSV");
+        } catch (Exception e) {
+            logger.error("Error with loading data from CSV");
+            e.printStackTrace();
+        }
+
     }
 }
