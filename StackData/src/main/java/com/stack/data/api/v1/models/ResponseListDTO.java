ae833e2351473b5a0c5b1ff0479be08f239a64e4
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2019-08-17 14:34:02 -0400

    ResponseListDTO keeps track of language and framework counts

--- StackData/src/main/java/com/stack/data/api/v1/models/ResponseListDTO.java
+++ StackData/src/main/java/com/stack/data/api/v1/models/ResponseListDTO.java
@@ -1,19 +1,58 @@
 package com.stack.data.api.v1.models;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ResponseListDTO {
 
     Integer responseCount;
+    Map<String, Integer> languages = new HashMap();
+    Map<String, Integer> frameworks = new HashMap();
     List<ResponseDTO> responseDTOS;
 
     public ResponseListDTO() {
     }
 
     public ResponseListDTO(List<ResponseDTO> responseDTOS) {
-
         this.responseDTOS = responseDTOS;
         this.responseCount = responseDTOS.size();
+        setLanguagesAndFrameworks();
+    }
+
+    private void setLanguagesAndFrameworks() {
+        for (ResponseDTO response : responseDTOS) {
+            for (String language : response.getLanguages()) {
+                languages.put(language, languages.getOrDefault(language, 0) + 1);
+            }
+            for (String framework : response.getFrameworks()) {
+                frameworks.put(framework, frameworks.getOrDefault(framework, 0) + 1);
+            }
+        }
+    }
+
+    public Integer getResponseCount() {
+        return responseCount;
+    }
+
+    public void setResponseCount(Integer responseCount) {
+        this.responseCount = responseCount;
+    }
+
+    public Map<String, Integer> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(Map<String, Integer> languages) {
+        this.languages = languages;
+    }
+
+    public Map<String, Integer> getFrameworks() {
+        return frameworks;
+    }
+
+    public void setFrameworks(Map<String, Integer> frameworks) {
+        this.frameworks = frameworks;
     }
 
     public Integer getResponses() {
--- StackData/src/main/java/com/stack/data/bootstrap/Bootstrap.java
+++ StackData/src/main/java/com/stack/data/bootstrap/Bootstrap.java
@@ -5,6 +5,9 @@ import com.stack.data.services.ResponseService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Component
 public class Bootstrap implements CommandLineRunner {
 
@@ -19,16 +22,33 @@ public class Bootstrap implements CommandLineRunner {
         ResponseDTO responseDTO = new ResponseDTO();
         responseDTO.setCountry("US");
         responseDTO.setDevType("New grad");
+        Set<String> languages = new HashSet<>();
+        languages.add("Java");
+        languages.add("Python");
+        responseDTO.setLanguages(languages);
+        Set<String> frameworks = new HashSet<>();
+        frameworks.add("Spring");
+        frameworks.add("Rails");
+        responseDTO.setFrameworks(frameworks);
         responseService.createNewResponse(responseDTO);
 
         ResponseDTO responseDTO2 = new ResponseDTO();
         responseDTO2.setCountry("CA");
         responseDTO2.setDevType("New grad");
+        Set<String> languages2 = new HashSet<>();
+        languages2.add("Java");
+        languages2.add("C++");
+        responseDTO2.setLanguages(languages2);
+        Set<String> frameworks2 = new HashSet<>();
+        frameworks2.add("Spring");
+        frameworks2.add("React");
+        responseDTO2.setFrameworks(frameworks2);
         responseService.createNewResponse(responseDTO2);
 
         ResponseDTO responseDTO3 = new ResponseDTO();
         responseDTO3.setCountry("CA");
         responseDTO3.setDevType("5 Years Experience");
+        responseDTO3.setFrameworks(frameworks2);
         responseService.createNewResponse(responseDTO3);
     }
 }
