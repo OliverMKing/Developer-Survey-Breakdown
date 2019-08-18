0c42efb8cd2e036cedd07682c5fa77035f414469
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2019-08-17 22:55:35 -0400

    Added graph to React and load graph with api data

--- stack-data-react/src/App.js
+++ stack-data-react/src/App.js
@@ -4,10 +4,10 @@ import GraphDisplay from "./GraphDisplay.js";
 function App() {
   return (
     <div className="container">
-      <div class="row justify-content-md-center">
+      <div className="row justify-content-md-center">
         <h1 className="display-4">Stack Data</h1>
       </div>
-      <div class="row justify-content-md-center">
+      <div className="row justify-content-md-center">
         <h4 className="lead text-muted">
           Breaking down the Stack Overflow 2018 Developer Survey
         </h4>
